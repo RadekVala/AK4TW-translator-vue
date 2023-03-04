d73d6a9b7fb1c37d3157c971a089d57331f6eb31
Radek Vala <radek.www@gmail.com>
2023-03-04 17:33:40 +0100

    Final commit with API fetch and history

--- my-vue-translator/src/views/HistoryView.vue
+++ my-vue-translator/src/views/HistoryView.vue
@@ -1,15 +1,45 @@
+<script>
+import { ref, onMounted } from "vue";
+
+export default {
+  setup() {
+    const items = ref([]);
+
+    onMounted(() => {
+      const historyLS = localStorage.getItem("history");
+      if (historyLS) {
+        items.value = JSON.parse(historyLS);
+      }
+    });
+   
+    
+    return {
+      items,
+    };
+  },
+};
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <ul class="list-group">
+          <li
+            v-for="(item, key) in items"
+            v-bind:key="key"
+            class="list-group-item"
+          >
+            {{ item }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<style scoped>
+  .list-group-item {
+    color: red;
   }
-}
 </style>
--- my-vue-translator/src/views/HomeView.vue
+++ my-vue-translator/src/views/HomeView.vue
@@ -1,4 +1,56 @@
-<script setup>
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const userInput = ref("");
+    let translationResult = ref("");
+
+    function handleClick() {
+      console.log("Submitted: " + userInput.value);
+      fetchData(userInput.value);
+    }
+
+    async function fetchData(userInput) {
+      /*
+      try {
+        const response = await fetch(
+          `https://api.mymemory.translated.net/get?q=${userInput}&langpair=en|cs`
+        );
+        const data = await response.json();
+        console.log(data);
+        translationResult.value = data.responseData.translatedText;
+
+        let history = [];
+        const historyLS = localStorage.getItem("history");
+        if (historyLS) {
+          history = JSON.parse(historyLS);
+        }
+
+        history.push(data.responseData.translatedText);
+        localStorage.setItem("history", JSON.stringify(history));
+      } catch (error) {
+        console.error(error);
+      }
+      */
+
+      fetch(`https://api.mymemory.translated.net/get?q=${userInput}&langpair=en|cs`)
+    .then(response => response.json())
+    .then(data => {
+        // Do something with the JSON data
+        console.log(data);
+        translationResult.value =  data.responseData.translatedText;
+    })
+    .catch(error => console.error(error));
+    }
+
+    return {
+      userInput,
+      handleClick,
+      translationResult,
+    };
+  },
+};
 </script>
 
 <template>
@@ -10,18 +62,18 @@
             <h3 class="card-title">Translator</h3>
 
             <input
-              id="userInput"
+              v-model="userInput"
               class="my-3 form-control form-control-lg"
               type="text"
               placeholder="Text for translation"
               aria-label="Text for translation"
             />
 
-            <button id="btnTranslate" type="button" class="btn btn-primary">
+            <button @click="handleClick" type="button" class="btn btn-primary">
               Translate
             </button>
 
-            <p id="translationResult"></p>
+            <p id="translationResult">{{ translationResult }}</p>
           </div>
         </div>
       </div>
