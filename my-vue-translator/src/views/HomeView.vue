bd5f82c9cb8e09fd994f00050d6fec2c3fc1927e
Radek Vala <radek.www@gmail.com>
2023-03-04 17:03:35 +0100

    Clean of the default project, basic UI for translator added

--- my-vue-translator/src/views/HomeView.vue
+++ my-vue-translator/src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-body">
+            <h3 class="card-title">Translator</h3>
+
+            <input
+              id="userInput"
+              class="my-3 form-control form-control-lg"
+              type="text"
+              placeholder="Text for translation"
+              aria-label="Text for translation"
+            />
+
+            <button id="btnTranslate" type="button" class="btn btn-primary">
+              Translate
+            </button>
+
+            <p id="translationResult"></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4">Reklama</div>
+    </div>
+  </div>
 </template>
